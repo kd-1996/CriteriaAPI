0230b05485fb40bead11543264f72dbaaa2cb376
kailash <kmoond1696@gmail.com>
2023-01-02 20:37:26 +0530

    implementation class of BookRepository interface at runtime

--- src/main/java/com/example/CriterialAPI/repositories/AuthorRepository.java
+++ src/main/java/com/example/CriterialAPI/repositories/AuthorRepository.java
@@ -4,4 +4,5 @@ import com.example.CriterialAPI.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
 }
--- src/test/java/com/example/CriterialAPI/CriterialApiApplicationTests.java
+++ src/test/java/com/example/CriterialAPI/CriterialApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.CriterialAPI;
 
+import com.example.CriterialAPI.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CriterialApiApplicationTests {
 
+	@Autowired
+	private BookRepository bookRepository;
+
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void repoTest(){
+		//TODO: It gives us the implementation class of BookRepository interface at runtime.
+
+		String className = bookRepository.getClass().getName();
+		String packageName = bookRepository.getClass().getPackageName();
+
+		System.out.println("class Name is"+className);
+		System.out.println("Package Name is"+packageName);
 	}
 
 }
